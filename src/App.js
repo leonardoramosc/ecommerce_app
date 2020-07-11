5e2eda899a3082116e74e1fe96d6fed43778cdc8
leonardoramosc <leorrc96@gmail.com>
2020-07-11 10:41:38 -0400

    Se dividio la homepage en varios componentes y se termino de estilizar los menu-items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './homepage.component.jsx';
+import HomePage from './components/pages/homepage/homepage.component.jsx';
 
 function App() {
   return (
---
+++ src/components/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Directory from '../../directory/directory.component.jsx';
+import './homepage.styles.scss';
+
+const HomePage = () => (
+	<div className="homepage">
+		<Directory />
+	</div>
+);
+
+export default HomePage;
