71cbb8da473e601b3c74762ea79f16a8433f27a4
leonardoramosc <leorrc96@gmail.com>
2020-07-12 07:03:06 -0400

    Se agrego componente de header y sus estilos correspondientes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 
 import HomePage from './components/pages/homepage/homepage.component.jsx';
 import ShopPage from './components/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
     <div >
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
