75cb8e6cd3cd5a3e218c7d79e564ba647c42982e
Leonardo Ramos <leorrc96@gmail.com>
2020-07-29 13:13:28 -0400

    Se creó el componente CartItem el cual sera mostrado en el CartDropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component'; 
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({cartHidden}) => (
+const CartDropdown = ({cartHidden, cartItems}) => {
+	return (
 	<div className='cart-dropdown'>
+
 		<div className="cart-items">
+			{
+				Object.keys(cartItems).map(key => <CartItem key={cartItems[key].id} item={cartItems[key]} />)
+			}
 		</div>
 
 		<CustomButton>GO TO CHECKOUT</CustomButton>
 	</div>
-);
+	)
+};
 
-export default CartDropdown;
+const mapStateToProps = ({cart: {cartItems}}) => ({
+	cartItems: cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
 
