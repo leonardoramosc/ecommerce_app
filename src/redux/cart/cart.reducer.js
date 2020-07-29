af74f67783f0d2b37abaf1f48664fc126daed69c
Leonardo Ramos <leorrc96@gmail.com>
2020-07-29 11:32:01 -0400

    Se agrego funcionalidad en cartReducer para agregar items al carrito y llevar el conteo de la cantidad y el total

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,19 +1,40 @@
 import {cartActionTypes} from './cart.type';
 
 const INITIAL_STATE = {
-	hideCart: true
+	hideCart: true,
+	cartItems: {},
+	total: 0
 }
 
 const cartReducer = (state=INITIAL_STATE, action) => {
+
+	const {id: itemID, price} = action.payload ? action.payload : {id:null, price:null};
+	const item = state.cartItems[itemID] ? state.cartItems[itemID] : null;
+	
 	switch(action.type){
 		case cartActionTypes.TOGGLE_HIDDEN_CART:
 			return {
 				...state,
 				hideCart: !state.hideCart
 			}
+		case cartActionTypes.ADD_ITEM:
+			return {
+				...state,
+				cartItems: {
+					...state.cartItems,
+					[itemID]: {
+						...action.payload,
+						quantity: item ? item.quantity+1 : 1,
+						total: item ? ((item.quantity+1) * price) : price
+					}
+				},
+				total: state.total + price
+			}
 		default:
 			return state;
 	}
 }
+
+
 
 export default cartReducer;
